6763bb9867597e97a57b242b69ec3598033d3a01
Kevin Wenger <wenger.kev@gmail.com>
2016-03-01 20:02:39 +0100

    animations before submit

--- web/app/Modules/authentication/login.js
+++ web/app/Modules/authentication/login.js
@@ -18,7 +18,13 @@ angular.module('anagram_hero.authentication', [
     $scope.check = function(user) {
         // Animate the arrow button
         $('.authentication--login--page .login--form--wrapper .login--form .login--form--input-send button .value').transition({ left: '100%', opacity: 0 }, 300, 'cubic-bezier(.65,.01,.72,.22)', function(){
-            $(this).css({left: 0}).transition({ left: '50%', delay: 400, opacity: 1}, 300, 'cubic-bezier(.65,.01,.72,.22)');
+            $(this).css({left: 0}).transition({ left: '50%', delay: 400, opacity: 1}, 300, 'cubic-bezier(.65,.01,.72,.22)', function(){
+                var result = AuthService.login(user).$promise.then(function(resp) {
+                    if( AuthService.isLoggedin() ){
+                        $window.location.href = '#home';
+                    }
+                });
+            });
         });
 
         // Animate the left cartridge
@@ -27,11 +33,7 @@ angular.module('anagram_hero.authentication', [
         // Animate the right cartridge
         // $('.authentication--login--page .hello--wrapper .right-background').transition({ right: '-50%' }, 300, 'cubic-bezier(.65,.01,.72,.22)');
 
-        /*var result = AuthService.login(user).$promise.then(function(resp) {
-            if( AuthService.isLoggedin() ){
-                $window.location.href = '#home';
-            }
-        });*/
+
     };
 
 }]);
